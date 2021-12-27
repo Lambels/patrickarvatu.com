bf9affe266663cc7efefb4a20109b2314029f275
Lambels <patrick.arvatu@yahoo.com>
2021-12-27 16:41:53 +0200

    implemented sub blog surface level logic

---
+++ sub_blog.go
@@ -0,0 +1,56 @@
+package pa
+
+import (
+	"context"
+	"time"
+)
+
+type SubBlog struct {
+	ID int `json:"id"`
+
+	BlogID int `json:"blogID"`
+
+	Title    string     `json:"title"`
+	Content  string     `json:"body"`
+	Comments []*Comment `json:"comments"`
+
+	CreatedAt time.Time `json:"createdAt"`
+	UpdatedAt time.Time `json:"updatedAt"`
+}
+
+func (s *SubBlog) Validate() error {
+	if s.Title == "" {
+		return Errorf(EINVALID, "title is a required field.")
+	}
+	if s.Content == "" {
+		return Errorf(EINVALID, "content is a required field.")
+	}
+
+	return nil
+}
+
+type SubBlogService interface {
+	FindSubBlogByID(ctx context.Context, id int) (*Blog, error)
+
+	FindSubBlogs(ctx context.Context, filter SubBlogFilter) ([]*Blog, int, error)
+
+	CreateSubBlog(ctx context.Context, blog *SubBlog) error
+
+	UpdateSubBlog(ctx context.Context, update SubBlogUpdate) (*Blog, error)
+
+	DeleteSubBlog(ctx context.Context, id int) error
+}
+
+type SubBlogFilter struct {
+	ID     *int    `json:"id"`
+	Title  *string `json:"title"`
+	BlogID *int    `json:"blogID"`
+
+	Offset int `json:"offset"`
+	Limit  int `json:"limit"`
+}
+
+type SubBlogUpdate struct {
+	Title   *string `json:"title"`
+	Content *string `json:"content"`
+}
