aafd0c3f5b1e5dafa7b3bbd25c2adab7f51ebf32
Lambels <patrick.arvatu@yahoo.com>
2021-12-27 16:41:29 +0200

    implemented error management

---
+++ error.go
@@ -0,0 +1,52 @@
+package pa
+
+import (
+	"errors"
+	"fmt"
+)
+
+const (
+	ECONFLICT       = "conflict"
+	EINTERNAL       = "internal"
+	EINVALID        = "invalid"
+	ENOTFOUND       = "not_found"
+	ENOTIMPLEMENTED = "not_implemented"
+	EUNAUTHORIZED   = "unauthorized"
+)
+
+type Error struct {
+	Code string
+
+	Message string
+}
+
+func (e *Error) Error() string {
+	return fmt.Sprintf("wtf error: code=%s message=%s", e.Code, e.Message)
+}
+
+func ErrorCode(err error) string {
+	var e *Error
+	if err == nil {
+		return ""
+	} else if errors.As(err, &e) {
+		return e.Code
+	}
+	return EINTERNAL
+}
+
+func ErrorMessage(err error) string {
+	var e *Error
+	if err == nil {
+		return ""
+	} else if errors.As(err, &e) {
+		return e.Message
+	}
+	return "Internal error."
+}
+
+func Errorf(code string, format string, args ...interface{}) *Error {
+	return &Error{
+		Code:    code,
+		Message: fmt.Sprintf(format, args...),
+	}
+}
