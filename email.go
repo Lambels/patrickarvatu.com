ce5f1aadc0c24e3ff85d6a804d6f5367e5aaebf8
Lambels <patrick.arvatu@yahoo.com>
2022-01-01 16:36:41 +0200

    email doc

--- email.go
+++ email.go
@@ -1,5 +1,7 @@
 package pa
 
+// EmailService represents a service which manages auth in the system.
 type EmailService interface {
+	// SendEmail will send a emails to the adresses provided in to.
 	SendEmail(to []string, body, subject string) error
 }
