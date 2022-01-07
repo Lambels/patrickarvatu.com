382cd0c338fda2f9792c96a3067b55876fdb8041
Lambels <patrick.arvatu@yahoo.com>
2022-01-07 19:58:21 +0200

    added session type

---
+++ session.go
@@ -0,0 +1,17 @@
+package pa
+
+/*
+	You could argue that the session types should be under the http package but I consider them important
+	enough to get their own package + you could implement a session cacher which would also need access
+	to the session type
+*/
+
+// SessionCookieName represents the name of the session cookie.
+const SessionCookieName = "session"
+
+// Session represents data stored per session under a secure cookie.
+type Session struct {
+	UserID      int    `json:"userID"`
+	RedirectURL string `json:"redirectURL"`
+	State       string `json:"state"`
+}
