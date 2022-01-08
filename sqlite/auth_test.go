47392709d9a8591c5041d21dcb405dc71c8b355f
Lambels <patrick.arvatu@yahoo.com>
2022-01-08 15:40:29 +0200

    ok call test auth

---
+++ sqlite/auth_test.go
@@ -0,0 +1,50 @@
+package sqlite_test
+
+import (
+	"context"
+	"reflect"
+	"testing"
+	"time"
+
+	pa "github.com/Lambels/patrickarvatu.com"
+	"github.com/Lambels/patrickarvatu.com/sqlite"
+)
+
+func TestCreateAuth(t *testing.T) {
+	t.Run("Ok Call", func(t *testing.T) {
+		db := MustOpenDB(t)
+		defer MustCloseDB(t, db)
+
+		backgroundCtx := context.Background()
+
+		authService := sqlite.NewAuthService(db)
+
+		time := time.Date(2022, time.January, 8, 0, 0, 0, 0, time.UTC)
+		auth := &pa.Auth{
+			User: &pa.User{ // make sure we dont provide uID indicating that we want to create a user.
+				ID:    1,
+				Name:  "Jhon",
+				Email: "jhon@doe.com",
+			},
+			Source:       "cool-source",
+			SourceID:     "cooler-source-id",
+			RefreshToken: "my-secret-token",
+			AccessToken:  "my-very-secret-token",
+			Expiry:       &time,
+		}
+
+		// create auth.
+		if err := authService.CreateAuth(backgroundCtx, auth); err != nil {
+			t.Fatal(err)
+		} else if auth.ID == 0 {
+			t.Fatal("got id = 0")
+		}
+
+		// assert creation
+		if gotAuth, err := authService.FindAuthByID(backgroundCtx, 1); err != nil {
+			t.Fatal(err)
+		} else if !reflect.DeepEqual(auth, gotAuth) {
+			t.Fatal("DeepEqual: auth != gotAuth")
+		}
+	})
+}
