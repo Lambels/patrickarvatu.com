1da1147abbaa90ac806385f1e5c236fc67f6ad10
Lambels <patrick.arvatu@yahoo.com>
2022-01-12 17:55:06 +0200

    sql logic implemented

--- sqlite/comment.go
+++ sqlite/comment.go
@@ -2,6 +2,7 @@ package sqlite
 
 import (
 	"context"
+	"strings"
 
 	pa "github.com/Lambels/patrickarvatu.com"
 )
@@ -129,22 +130,159 @@ func findCommentByID(ctx context.Context, tx *Tx, id int) (*pa.Comment, error) {
 	return comments[0], nil
 }
 
-func findComments(ctx context.Context, tx *Tx, filter pa.CommentFilter) ([]*pa.Comment, int, error) {
+func findComments(ctx context.Context, tx *Tx, filter pa.CommentFilter) (_ []*pa.Comment, n int, err error) {
+	// build where and args statement method.
+	// not vulnerable to sql injection attack.
+	where, args := []string{"1 = 1"}, []interface{}{}
 
-}
+	if v := filter.ID; v != nil {
+		where = append(where, "id = ?")
+		args = append(args, *v)
+	}
+	if v := filter.SubBlogID; v != nil {
+		where = append(where, "sub_blog_id = ?")
+		args = append(args, *v)
+	}
+	if v := filter.UserID; v != nil {
+		where = append(where, "user_id = ?")
+		args = append(args, *v)
+	}
 
-func attachUserToComment(ctx context.Context, tx *Tx, comment *pa.Comment) error {
+	rows, err := tx.QueryContext(ctx, `
+		SELECT
+			id,
+			sub_blog_id,
+			user_id,
+			content,
+			created_at,
+			COUNT(*) OVER()
+		FROM comments
+		WHERE`+strings.Join(where, " AND ")+`
+		ORDER BY id ASC
+		`+FormatLimitOffset(filter.Limit, filter.Offset)+`
+	`,
+		args...,
+	)
 
+	if err != nil {
+		return nil, n, err
+	}
+
+	// deserialize rows.
+	comments := []*pa.Comment{}
+	for rows.Next() {
+		var comment *pa.Comment
+
+		if err := rows.Scan(
+			&comment.ID,
+			&comment.SubBlogID,
+			&comment.UserID,
+			&comment.Content,
+			(*NullTime)(&comment.CreatedAt),
+			&n,
+		); err != nil {
+			return nil, 0, err
+		}
+
+		comments = append(comments, comment)
+	}
+	if err := rows.Err(); err != nil {
+		return nil, 0, err
+	}
+
+	return comments, n, nil
 }
 
 func createComment(ctx context.Context, tx *Tx, comment *pa.Comment) error {
+	comment.CreatedAt = tx.now
 
+	if err := comment.Validate(); err != nil {
+		return err
+	}
+
+	result, err := tx.ExecContext(ctx, `
+		INSERT INTO comments (
+			sub_blog_id,
+			user_id,
+			content,
+			created_at,
+		)
+		VALUES(?, ?, ?, ?)
+	`,
+		comment.SubBlogID,
+		comment.UserID,
+		comment.Content,
+		(*NullTime)(&comment.CreatedAt),
+	)
+
+	if err != nil {
+		return err
+	}
+
+	id, err := result.LastInsertId()
+	if err != nil {
+		return err
+	}
+
+	// set id from database to comment obj.
+	comment.ID = int(id)
+	return nil
 }
 
 func updateComment(ctx context.Context, tx *Tx, id int, update pa.CommentUpdate) (*pa.Comment, error) {
+	if !pa.IsAdminContext(ctx) {
+		return nil, pa.Errorf(pa.EUNAUTHORIZED, "user isnt admin.")
+	}
 
+	comment, err := findCommentByID(ctx, tx, id)
+	if err != nil {
+		return nil, err
+	}
+
+	if v := update.Content; v != nil {
+		comment.Content = *v
+	}
+
+	if err := comment.Validate(); err != nil {
+		return comment, err
+	}
+
+	if _, err := tx.ExecContext(ctx, `
+		UPDATE comments,
+		SET content = ?,
+		WHERE id = ?
+	`,
+		comment.Content,
+		id,
+	); err != nil {
+		return nil, err
+	}
+
+	return comment, nil
 }
 
 func deleteComment(ctx context.Context, tx *Tx, id int) error {
+	comment, err := findCommentByID(ctx, tx, id)
+	if err != nil {
+		return err
+	}
 
+	if comment.UserID != pa.UserIDFromContext(ctx) {
+		return pa.Errorf(pa.EUNAUTHORIZED, "user cant delete comment")
+	}
+
+	if _, err := tx.ExecContext(ctx, `DELETE FROM comments WHERE id = ?`, id); err != nil {
+		return err
+	}
+	return nil
+}
+
+func attachUserToComment(ctx context.Context, tx *Tx, comment *pa.Comment) error {
+	user, err := findUserByID(ctx, tx, comment.UserID)
+	if err != nil {
+		return err
+	}
+
+	comment.User = user
+	return nil
 }
