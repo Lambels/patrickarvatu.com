4e47802ea8e9b1d47f9fbace6f6be5a4b4b09c2f
Lambels <patrick.arvatu@yahoo.com>
2022-01-13 21:32:37 +0200

    fixed sql syntax

--- sqlite/migration/00000002.sql
+++ sqlite/migration/00000002.sql
@@ -37,7 +37,8 @@ CREATE TABLE sub_blogs (
     blog_id     INTEGER NOT NULL REFERENCES blogs (id) ON DELETE CASCADE,
     title       TEXT NOT NULL,
     content     TEXT NOT NULL,
-    created_at  TEXT NOT NULL
+    created_at  TEXT NOT NULL,
+    updated_at  TEXT NOT NULL
 );
 
 CREATE TABLE comments (
@@ -49,13 +50,13 @@ CREATE TABLE comments (
 );
 
 CREATE TABLE blog_subscriptions (
-    id          INTEGER PRIMARY KEY AUTOINCREMENT
+    id          INTEGER PRIMARY KEY AUTOINCREMENT,
     user_id     INTEGER NOT NULL REFERENCES users (id) ON DELETE CASCADE,
-    blog_id     INTEGER NOT NULL REFERENCES blogs (id) ON DELETE CASCADE,
+    blog_id     INTEGER NOT NULL REFERENCES blogs (id) ON DELETE CASCADE
 );
 
 CREATE TABLE sub_blog_subscriptions (
-    id          INTEGER PRIMARY KEY AUTOINCREMENT
+    id          INTEGER PRIMARY KEY AUTOINCREMENT,
     sub_blog_id INTEGER NOT NULL REFERENCES sub_blogs (id) ON DELETE CASCADE,
-    user_id     INTEGER NOT NULL REFERENCES users (id) ON DELETE CASCADE,
+    user_id     INTEGER NOT NULL REFERENCES users (id) ON DELETE CASCADE
 );
