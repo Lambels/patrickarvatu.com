d8172785782b4021c58fe4a3238de4475c149b8e
Lambels <patrick.arvatu@yahoo.com>
2022-01-15 17:32:37 +0200

    update blog functionality test passing

--- sqlite/blog_test.go
+++ sqlite/blog_test.go
@@ -92,7 +92,7 @@ func TestDeleteBlog(t *testing.T) {
 			Name:    "Jhon Doe",
 			Email:   "jhon@doe.com",
 			IsAdmin: true,
-		} // no need to create user as CreateBlog doesent check any keys.
+		} // no need to create user as DeleteBlog doesent check any keys.
 
 		adminUsrContext := pa.NewContextWithUser(backgroundCtx, user)
 
@@ -129,7 +129,7 @@ func TestDeleteBlog(t *testing.T) {
 			Name:    "Jhon Doe",
 			Email:   "jhon@doe.com",
 			IsAdmin: true,
-		} // no need to create user as CreateBlog doesent check any keys.
+		} // no need to create user as DeleteBlog doesent check any keys.
 
 		user2 := &pa.User{
 			Name:  "Lambels",
@@ -167,7 +167,7 @@ func TestDeleteBlog(t *testing.T) {
 			Name:    "Jhon Doe",
 			Email:   "jhon@doe.com",
 			IsAdmin: true,
-		} // no need to create user as CreateBlog doesent check any keys.
+		} // no need to create user as DeleteBlog doesent check any keys.
 
 		adminUsrContext := pa.NewContextWithUser(backgroundCtx, user)
 
@@ -180,4 +180,113 @@ func TestDeleteBlog(t *testing.T) {
 
 func TestUpdateBlog(t *testing.T) {
 	t.Parallel()
+	t.Run("Ok Update Call", func(t *testing.T) {
+		db := MustOpenTempDB(t)
+		defer MustCloseDB(t, db)
+
+		backgroundCtx := context.Background()
+
+		blogService := sqlite.NewBlogService(db)
+
+		user := &pa.User{
+			Name:    "Jhon Doe",
+			Email:   "jhon@doe.com",
+			IsAdmin: true,
+		} // no need to create user as UpdateBlog doesent check any keys.
+
+		adminUsrContext := pa.NewContextWithUser(backgroundCtx, user)
+
+		blog := &pa.Blog{
+			Title:       "Epic Blog",
+			Description: "Honestly the best blog ever.",
+		}
+		update := pa.BlogUpdate{
+			Title:       NewStringPointer("Bad Blog"),
+			Description: NewStringPointer("Honestly worst blog ever"),
+		}
+
+		// create blog.
+		if err := blogService.CreateBlog(adminUsrContext, blog); err != nil {
+			t.Fatal(err)
+		}
+
+		// update blog.
+		if updatedBlog, err := blogService.UpdateBlog(adminUsrContext, 1, update); err != nil {
+			t.Fatal(err)
+		} else if gotBlog, err := blogService.FindBlogByID(backgroundCtx, 1); err != nil { // assert update.
+			t.Fatal(err)
+		} else if !reflect.DeepEqual(updatedBlog, gotBlog) {
+			t.Log(*updatedBlog, *gotBlog)
+			t.Fatal("DeepEqual: updatedBlog != gotBlog")
+		}
+	})
+
+	t.Run("Bad Update Call (Un Auth)", func(t *testing.T) {
+		db := MustOpenTempDB(t)
+		defer MustCloseDB(t, db)
+
+		backgroundCtx := context.Background()
+
+		blogService := sqlite.NewBlogService(db)
+
+		user := &pa.User{
+			Name:    "Jhon Doe",
+			Email:   "jhon@doe.com",
+			IsAdmin: true,
+		} // no need to create user as DeleteBlog doesent check any keys.
+
+		user2 := &pa.User{
+			Name:  "Lambels",
+			Email: "Lamb@Lambels.com",
+		}
+
+		adminUsrContext := pa.NewContextWithUser(backgroundCtx, user)
+		user2Context := pa.NewContextWithUser(backgroundCtx, user2)
+
+		blog := &pa.Blog{
+			Title:       "Epic Blog",
+			Description: "Honestly the best blog ever.",
+		}
+		update := pa.BlogUpdate{
+			Title:       NewStringPointer("Bad Blog"),
+			Description: NewStringPointer("Honestly worst blog ever"),
+		}
+
+		// create blog.
+		if err := blogService.CreateBlog(adminUsrContext, blog); err != nil {
+			t.Fatal(err)
+		}
+
+		// update blog (Un Auth).
+		if _, err := blogService.UpdateBlog(user2Context, 1, update); pa.ErrorCode(err) != pa.EUNAUTHORIZED {
+			t.Fatal("err != UNAUTHORIZED")
+		}
+	})
+
+	t.Run("Bad Update Call (Not Found)", func(t *testing.T) {
+		db := MustOpenTempDB(t)
+		defer MustCloseDB(t, db)
+
+		backgroundCtx := context.Background()
+
+		blogService := sqlite.NewBlogService(db)
+
+		user := &pa.User{
+			Name:    "Jhon Doe",
+			Email:   "jhon@doe.com",
+			IsAdmin: true,
+		} // no need to create user as DeleteBlog doesent check any keys.
+
+		adminUsrContext := pa.NewContextWithUser(backgroundCtx, user)
+
+		update := pa.BlogUpdate{
+			Title:       NewStringPointer("Bad Blog"),
+			Description: NewStringPointer("Honestly worst blog ever"),
+		}
+
+		// update blog (Un Auth).
+		if _, err := blogService.UpdateBlog(adminUsrContext, 1, update); pa.ErrorCode(err) != pa.ENOTFOUND {
+			t.Fatal("err != ENOTFOUND")
+		}
+	})
 }
