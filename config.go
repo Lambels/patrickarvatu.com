35bbf38a4afb29e64578c9b7877b426bc7e96acf
Lambels <patrick.arvatu@yahoo.com>
2022-01-22 14:09:36 +0200

    added toml config type (change location to main?)

---
+++ config.go
@@ -0,0 +1,16 @@
+package pa
+
+type Config struct {
+	Github struct {
+		ClientID     string `toml:"client-id"`
+		ClientSecret string `toml:"client-secret"`
+	} `toml:"github"`
+
+	HTTP struct {
+		Addr        string `toml:"addr"`
+		Domain      string `toml:"domain"`
+		BlockKey    string `toml:"block-key"`
+		HashKey     string `toml:"hash-key"`
+		FrontendURL string `toml:"frontend-url"`
+	} `toml:"http"`
+}
