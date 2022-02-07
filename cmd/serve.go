d0846d8e8cc157f8595556b57e566326a8331eed
Lambels <patrick.arvatu@yahoo.com>
2022-02-07 12:31:21 +0200

    debug serve option

--- cmd/serve.go
+++ cmd/serve.go
@@ -7,6 +7,7 @@ import (
 	"syscall"
 
 	pa "github.com/Lambels/patrickarvatu.com"
+	"github.com/Lambels/patrickarvatu.com/http"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
@@ -20,9 +21,11 @@ var serveCmd = &cobra.Command{
 
 func init() {
 	rootCmd.AddCommand(serveCmd)
+
+	serveCmd.Flags().BoolP("debug", "d", false, "starts a prom-http on port :8000 /metrics.")
 }
 
-func RunServe(_ *cobra.Command, _ []string) error {
+func RunServe(cmd *cobra.Command, _ []string) error {
 	var cfg pa.Config
 
 	if err := viper.Unmarshal(&cfg); err != nil {
@@ -34,6 +37,11 @@ func RunServe(_ *cobra.Command, _ []string) error {
 		return err
 	}
 	defer cleanUp()
+
+	if debug, _ := cmd.Flags().GetBool("debug"); debug {
+		go http.RunDebugServer()
+		log.Println("[INFO] Started debug server on :8000 /metrics.")
+	}
 
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
