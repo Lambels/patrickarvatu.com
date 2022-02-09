8db8109a261085a0711c3ba1a1b55fac6d2df44c
Lambels <patrick.arvatu@yahoo.com>
2022-02-09 17:36:16 +0200

    wrapped app to auth provider

--- frontend/src/pages/_app.jsx
+++ frontend/src/pages/_app.jsx
@@ -1,7 +1,12 @@
 import "tailwindcss/tailwind.css";
+import AuthProvider from "../store/auth-context";
 
 function Application({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <AuthProvider>
+      <Component {...pageProps} />
+    </AuthProvider>
+  )
 }
 
 export default Application
