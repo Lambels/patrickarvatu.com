251ebf2db5c50c23c569e977eda0564b4295cb85
Lambels <patrick.arvatu@yahoo.com>
2022-02-10 20:57:58 +0200

    added hover animation

--- frontend/src/components/project/project.jsx
+++ frontend/src/components/project/project.jsx
@@ -1,6 +1,6 @@
 function Project({ project }) {
   return (
-    <a href={project.html_url} className="block max-w-lg bg-white rounded-lg border-2 border-gray-200 shadow-xl hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+    <a href={project.html_url} className="block max-w-lg bg-white rounded-lg border-2 border-gray-200 shadow-xl hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-109 duration-300">
       <img className="w-full h-60 pb-1 rounded-lg" src={`http://localhost:3000/image/${project.name}_PROJECT.jpg`} alt="Project Image"/>
       <div className="p-3">
         <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{project.name}</h5>
