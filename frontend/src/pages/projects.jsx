2e7b19064b02d1ff3d610e49fed40db42bd6d9c6
Lambels <patrick.arvatu@yahoo.com>
2022-02-10 13:13:19 +0200

    project page

--- frontend/src/pages/projects.jsx
+++ frontend/src/pages/projects.jsx
@@ -1,21 +1,25 @@
 import ProjectLayout from "../components/projects-layout";
 import Project from "../components/project";
+import { NextSeo } from "next-seo";
 
 function Projects({ data }) {
-    return (
-        <ProjectLayout>
-            {data.map((obj) => {
-                return <Project project={obj}/>
-            })}
-        </ProjectLayout>
-    )
+  return (
+    <>
+      <NextSeo title="Projects" />
+      <ProjectLayout>
+        {data.map((obj) => {
+          return <Project project={obj} />;
+        })}
+      </ProjectLayout>
+    </>
+  );
 }
 
 export async function getServerSideProps() {
-    const response = await fetch("https://api.github.com/users/Lambels/repos");
+  const response = await fetch("https://api.github.com/users/Lambels/repos");
 
-    const data = await response.json();
-    return { props: { data } }
+  const data = await response.json();
+  return { props: { data } };
 }
 
-export default Projects
+export default Projects;
