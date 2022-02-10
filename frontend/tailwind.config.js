8a788e7c4b0d7848e089c35979ca15b6e384565d
Lambels <patrick.arvatu@yahoo.com>
2022-02-10 20:19:45 +0200

    tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,9 @@ module.exports = {
   mode: "jit",
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      "waves-1": "url('/waves1.svg')"
+    },
   },
   plugins: [],
 }
