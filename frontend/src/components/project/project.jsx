499f9aefc483291a23caee7f2ebf758568a0f9bc
Lambels <patrick.arvatu@yahoo.com>
2022-02-10 13:09:22 +0200

    project component finished

--- frontend/src/components/project/project.jsx
+++ frontend/src/components/project/project.jsx
@@ -1,21 +1,19 @@
 function Project({ project }) {
   return (
-    <div className="rounded overflow-hidden shadow-lg">
-      <img className="w-full" src={`http://localhost:3000/image/${project.name}_PROJECT.jpg`} alt="Project Image" />
-      <div className="px-6 py-4">
-        <div className="font-bold text-xl mb-2">{project.name}</div>
-        <p className="text-gray-700 text-base">{project.description}</p>
+    <a href={project.html_url} className="block max-w-lg bg-white rounded-lg border-2 border-gray-200 shadow-xl hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+      <img className="w-full h-60 pb-1 rounded-lg" src={`http://localhost:3000/image/${project.name}_PROJECT.jpg`} alt="Project Image"/>
+      <div className="p-3">
+        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{project.name}</h5>
+        <p className="font-normal text-gray-700 dark:text-gray-400">{project.description}</p>
       </div>
-      <div className="px-6 pt-4 pb-2">
-        {project.topics.map((tag) => {
+      <div className="p-3 pb-4">
+        {project.topics.map((obj, i) => {
           return (
-            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-              {tag}
-            </span>
-          );
+            <span key={i} className="inline-block p1 pl-2 pr-2 mr-2 rounded-xl bg-gray-700 border-gray-600 border text-white font-semibold">{"#"+obj}</span>
+          )
         })}
       </div>
-    </div>
+    </a>
   );
 }
 
