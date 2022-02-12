b31e1e5befe054b58befaf7905504736cd63ed60
Lambels <patrick.arvatu@yahoo.com>
2022-02-12 14:32:51 +0200

    removed header from app layout

--- frontend/src/components/app-layout/app-layout.jsx
+++ frontend/src/components/app-layout/app-layout.jsx
@@ -1,9 +1,6 @@
-import Header from "../header/header";
-
 function AppLayout({ children }) {
   return (
   <div className="min-h-screen bg-cover bg-no-repeat bg-center bg-[url('/waves1.svg')]" id="main">
-    <Header />
     {children}
   </div>
   );
