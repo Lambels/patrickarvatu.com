5371b672bc291b24151987d7154cd796c335ef75
Lambels <patrick.arvatu@yahoo.com>
2022-02-13 20:29:39 +0200

    waves support

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,7 +3,11 @@ module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      "waves-1": "url('/waves1.svg')"
+      "waves-0": "url('/waves0.svg')",
+      "waves-1": "url('/waves1.svg')",
+      "waves-2": "url('/waves2.svg')",
+      "waves-3": "url('/waves3.svg')",
+      "waves-4": "url('/waves4.svg')"
     },
   },
   plugins: [],
