c48ea1e722ed66457574b32327733b7c002b26ac
Lambels <patrick.arvatu@yahoo.com>
2022-02-13 18:24:43 +0200

    typo

--- frontend/src/pages/index.jsx
+++ frontend/src/pages/index.jsx
@@ -64,7 +64,7 @@ function Home({ showModalParam }) {
                   Day to day life
                 </h5>
                 <p className="font-normal text-gray-700 dark:text-gray-400">
-                  Im a bussy guy, I play hockey professionally and aim to keep
+                  Im a busy guy, I play hockey professionally and aim to keep
                   my grades up, when Im not coding im either playing sports or
                   studying. I also love reading when I have time ;)
                 </p>
