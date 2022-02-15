e1c17a513f32bfef449c1c5f51a1501a1e4b95d4
Lambels <patrick.arvatu@yahoo.com>
2022-02-15 15:49:34 +0200

    project surface level implemented, no sql

--- sqlite/project.go
+++ sqlite/project.go
@@ -22,23 +22,103 @@ func NewProjectService(db *DB) *ProjectService {
 }
 
 func (s *ProjectService) FindProjectByID(ctx context.Context, id int) (*pa.Project, error) {
+	tx, err := s.db.BeginTX(ctx, nil)
+	if err != nil {
+		return nil, err
+	}
+	defer tx.Rollback()
 
+	proj, err := findProjectByID(ctx, tx, id)
+	if err != nil {
+		return nil, err
+
+	} else if err = attachTopicsToProject(ctx, tx, proj); err != nil { // attach topics.
+
+		return nil, err
+	}
+
+	return proj, nil
 }
 
 func (s *ProjectService) FindProjectByName(ctx context.Context, name string) (*pa.Project, error) {
+	tx, err := s.db.BeginTX(ctx, nil)
+	if err != nil {
+		return nil, err
+	}
+	defer tx.Rollback()
 
+	proj, err := findProjectByName(ctx, tx, name)
+	if err != nil {
+		return nil, err
+
+	} else if err = attachTopicsToProject(ctx, tx, proj); err != nil { // attach topics.
+
+		return nil, err
+	}
+
+	return proj, nil
 }
 
 func (s *ProjectService) FindProjects(ctx context.Context, filter pa.ProjectFilter) ([]*pa.Project, int, error) {
+	tx, err := s.db.BeginTX(ctx, nil)
+	if err != nil {
+		return nil, 0, err
+	}
+	defer tx.Rollback()
 
+	projects, n, err := findProjects(ctx, tx, filter)
+	if err != nil {
+		return projects, n, err
+	}
+
+	// loop over each project.
+	for _, project := range projects {
+		if err := attachTopicsToProject(ctx, tx, project); err != nil { // attach topics.
+			return projects, n, err
+		}
+	}
+
+	return projects, n, err
 }
 
 func (s *ProjectService) CreateOrUpdateProject(ctx context.Context, project *pa.Project) error {
+	tx, err := s.db.BeginTX(ctx, nil)
+	if err != nil {
+		return err
+	}
+	defer tx.Rollback()
 
+	proj, err := findProjectByName(ctx, tx, project.Name)
+	switch pa.ErrorCode(err) {
+	case pa.ENOTFOUND: // (project doesent exist)
+		if err := createProject(ctx, tx, project); err != nil {
+			return err
+		}
+
+	case "": // nil error ErrorCode is an empty string. (project exists)
+		if err := updateProject(ctx, tx, proj.ID, project); err != nil {
+			return err
+		}
+
+	default: // error code pa.EINTERNAL, could be replaced by `case pa.EINTERNAL:` yet this code is more idiomatic.
+		return err
+	}
+
+	return tx.Commit()
 }
 
-func (s *ProjectService) DeleteProject(ctx context.Context, title string) error {
+func (s *ProjectService) DeleteProject(ctx context.Context, name string) error {
+	tx, err := s.db.BeginTX(ctx, nil)
+	if err != nil {
+		return err
+	}
+	defer tx.Rollback()
 
+	if err := deleteProject(ctx, tx, name); err != nil {
+		return err
+	}
+
+	return tx.Commit()
 }
 
 // projects interface functions ---------------------------------------------------------
@@ -76,35 +156,41 @@ func findProjectByName(ctx context.Context, tx *Tx, name string) (*pa.Project, e
 }
 
 func findProjects(ctx context.Context, tx *Tx, filter pa.ProjectFilter) (_ []*pa.Project, n int, err error) {
-
+	return nil, 0, nil
 }
 
+// TODO: handle topics
 func createProject(ctx context.Context, tx *Tx, project *pa.Project) error {
-
+	return nil
 }
 
 func updateProject(ctx context.Context, tx *Tx, id int, project *pa.Project) error {
-
+	return nil
 }
 
 func deleteProject(ctx context.Context, tx *Tx, name string) error {
-
+	return nil
 }
 
 // topics: many 2 many interface functions ----------------------------------------------
+// to not be used directly.
 
-func createNewTopicDescription(ctx context.Context, tx *Tx, content string) error {
-
+func createNewTopic(ctx context.Context, tx *Tx, content string) error {
+	return nil
 }
 
-func createNewTopicLink(ctx context.Context, topicDescID, projectID int) error {
-
+func findTopicByContent(ctx context.Context, tx *Tx, content string) (*pa.Topic, error) {
+	return nil, nil
 }
 
-func findTopicsByProjectID(ctx context.Context, tx *Tx, id int) []string {
+func createNewTopicLink(ctx context.Context, tx *Tx, topicLink *pa.TopicLink) error {
+	return nil
+}
 
+func findTopicsByProjectID(ctx context.Context, tx *Tx, id int) ([]*pa.Topic, error) {
+	return nil, nil
 }
 
 func attachTopicsToProject(ctx context.Context, tx *Tx, project *pa.Project) error {
-
+	return nil
 }
