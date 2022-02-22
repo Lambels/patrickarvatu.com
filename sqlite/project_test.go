c625d290171be02c8c940d14e50db5cb7756a4ca
Lambels <patrick.arvatu@yahoo.com>
2022-02-22 19:04:43 +0200

    added ok create or update passing test

--- sqlite/project_test.go
+++ sqlite/project_test.go
@@ -1,3 +1,146 @@
 package sqlite_test
 
-// TODO: write tests.
+import (
+	"context"
+	"reflect"
+	"testing"
+
+	pa "github.com/Lambels/patrickarvatu.com"
+	"github.com/Lambels/patrickarvatu.com/sqlite"
+)
+
+// TODO: go with debugger in depth through each step to observe behaviour.
+// TODO: test all methods.
+
+func TestCreateOrUpdateProject(t *testing.T) {
+	t.Run("Ok Create or Update Call", func(t *testing.T) {
+		db := MustOpenTempDB(t)
+		defer MustCloseDB(t, db)
+
+		backgroundCtx := context.Background()
+
+		user := &pa.User{
+			Name:    "Jhon Doe",
+			Email:   "jhon@doe.com",
+			IsAdmin: true,
+		} // no need to create user as CreateOrUpdateProject doesent check any keys.
+
+		adminUsrCtx := pa.NewContextWithUser(backgroundCtx, user)
+
+		projectService := sqlite.NewProjectService(db)
+
+		t.Run("step 1", func(t *testing.T) {
+			// current projects: []
+			pj1 := &pa.Project{
+				Name:        "patrickarvatu.com",
+				Description: "Some description",
+				Topics: []string{
+					"go",
+				},
+				HtmlURL: "https://github.com/Lambels/patrickarvatu.com",
+			}
+
+			// create project.
+			if err := projectService.CreateOrUpdateProject(adminUsrCtx, pj1); err != nil {
+				t.Fatal(err)
+			} else if pj1.ID == 0 {
+				t.Fatal("got id = 0")
+			}
+
+			// assert creation.
+			if gotProject, err := projectService.FindProjectByID(backgroundCtx, pj1.ID); err != nil {
+				t.Fatal(err)
+			} else if !reflect.DeepEqual(pj1, gotProject) {
+				t.Fatal("DeepEqual: gotProject != pj1")
+			}
+		})
+
+		t.Run("step 2", func(t *testing.T) {
+			// current projects: [patrickarvatu.com]
+
+			pj1 := &pa.Project{
+				Name:        "patrickarvatu.com",
+				Description: "Some other description",
+				Topics: []string{
+					"go",
+					"web dev",
+					"js",
+					"golang",
+				},
+				HtmlURL: "https://github.com/Lambels/patrickarvatu.com",
+			}
+
+			// update project 1.
+			if err := projectService.CreateOrUpdateProject(adminUsrCtx, pj1); err != nil {
+				t.Fatal(err)
+			}
+
+			// for reflect.DeepEqual test.
+			pj1.ID = 1
+
+			// assert update.
+			if gotProject, err := projectService.FindProjectByID(backgroundCtx, 1); err != nil {
+				t.Fatal(err)
+			} else if !reflect.DeepEqual(pj1, gotProject) {
+				t.Fatal("DeepEqual: gotProject != pj1")
+			}
+		})
+
+		t.Run("step 3", func(t *testing.T) {
+			// current projects: [patrickarvatu.com]
+			pj1 := &pa.Project{
+				Name:        "Lambels",
+				Description: "No description :(",
+				HtmlURL:     "https://github.com/Lambels/Lambels",
+			}
+
+			// create project.
+			if err := projectService.CreateOrUpdateProject(adminUsrCtx, pj1); err != nil {
+				t.Fatal(err)
+			} else if pj1.ID == 0 {
+				t.Fatal("got id = 0")
+			}
+
+			// assert creation.
+			if gotProject, err := projectService.FindProjectByID(backgroundCtx, pj1.ID); err != nil {
+				t.Fatal(err)
+			} else if !reflect.DeepEqual(pj1, gotProject) {
+				t.Fatal("DeepEqual: gotProject != pj1")
+			}
+		})
+
+		t.Run("step 4", func(t *testing.T) {
+			// current projects: [patrickarvatu.com, Lambels]
+
+			pj1, err := projectService.FindProjectByID(backgroundCtx, 1)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			pj2, err := projectService.FindProjectByID(backgroundCtx, 2)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			// assert previous steps.
+			if projects, n, err := projectService.FindProjects(backgroundCtx, pa.ProjectFilter{}); err != nil {
+				t.Fatal(err)
+			} else if n != 2 {
+				t.Fatal("n != 2")
+			} else if len(projects) != 2 {
+				t.Fatal("len(projects) != 2")
+			} else if !reflect.DeepEqual(projects[0], pj1) {
+				t.Fatal("DeepEqual: projects[0] != pj1")
+			} else if !reflect.DeepEqual(projects[1], pj2) {
+				t.Fatal("DeepEqual: projects[1] != pj2")
+			}
+		})
+	})
+}
+
+func MustCreateOrUpdateProject(t *testing.T, db *sqlite.DB, ctx context.Context, project *pa.Project) {
+	t.Helper()
+	if err := sqlite.NewProjectService(db).CreateOrUpdateProject(ctx, project); err != nil {
+		t.Fatal(err)
+	}
+}
