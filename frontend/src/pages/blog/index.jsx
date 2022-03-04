6a5912c12793566cb7f2689232281cc2ab729910
Lambels <patrick.arvatu@yahoo.com>
2022-03-04 19:42:01 +0200

    index

--- frontend/src/pages/blog/index.jsx
+++ frontend/src/pages/blog/index.jsx
@@ -1,5 +1,42 @@
-function Index(props) {
-    
+import { NextSeo } from "next-seo";
+import AdminBlogPreview from "../../components/admin-blog-preview/admin-blog-preview";
+import BlogPreview from "../../components/blog-preview/blog-preview";
+import BlogsPreviewLayout from "../../components/blogs-preview-layout";
+import { useAuth } from "../../store/auth-context";
+
+function Index({ blogData }) {
+  const { isAdmin } = useAuth();
+
+  if (isAdmin) {
+    return (
+      <>
+        <NextSeo title="blogs" />
+        <BlogsPreviewLayout>
+          {data.blogs?.map((obj, i) => {
+            return <AdminBlogPreview blog={obj} key={i} />;
+          })}
+        </BlogsPreviewLayout>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <NextSeo title="blogs" />
+        <BlogsPreviewLayout>
+          {blogData.blogs?.map((obj, i) => {
+              return <BlogPreview blog={obj} key={i} />;
+          })}
+        </BlogsPreviewLayout>
+      </>
+    );
+  }
 }
 
-export default Index
+export async function getServerSideProps() {
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/v1/blogs`);
+
+  const blogData = await response.json();
+  return { props: { blogData } };
+}
+
+export default Index;
