c57052ea86ac100f2da9019349114558a3fdd9da
Lambels <patrick.arvatu@yahoo.com>
2022-03-04 19:40:48 +0200

    preview layout component

---
+++ frontend/src/components/blogs-preview-layout/blog-preview-layout.jsx
@@ -0,0 +1,9 @@
+function BlogsPreviewLayout({ children }) {
+    return (
+        <div className="flex flex-col items-center p-6">
+            {children}
+        </div>
+    )
+}
+
+export default BlogsPreviewLayout
---
+++ frontend/src/components/blogs-preview-layout/index.js
@@ -0,0 +1,3 @@
+import BlogsPreviewLayout from "./blog-preview-layout";
+
+export default BlogsPreviewLayout
