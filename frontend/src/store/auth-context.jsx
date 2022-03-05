45881819bf56a4e5db6be32306a4a10e825877e3
Lambels <patrick.arvatu@yahoo.com>
2022-03-05 16:35:59 +0200

    changed auth api to use async await

--- frontend/src/store/auth-context.jsx
+++ frontend/src/store/auth-context.jsx
@@ -2,28 +2,35 @@ import { useRouter } from "next/router";
 import { createContext, useContext, useEffect, useState } from "react";
 
 const AuthContext = createContext({
-  data: undefined,
+  user: {},
   isAuth: false,
+  isAdmin: false,
+  pfpUrl: "",
   logout: () => {},
   redirectToProvider: () => {},
   updateUser: () => {},
 });
 
 function AuthProvider({ children }) {
-  const [data, setData] = useState({});
-  const [isAuth, setIsAuth] = useState(false);
+  const [userData, setUserData] = useState({
+    user: {},
+    isAuth: false,
+    isAdmin: false,
+  });
   const router = useRouter();
-
-  useEffect(() => {
-    updateUser();
-  }, []);
 
   const logout = () => {
     fetch(`${process.env.NEXT_PUBLIC_API_URL}/v1/oauth/user/logout`, {
       method: "DELETE",
       credentials: "include",
     }).then((response) => {
-      if (response.ok) setIsAuth(false);
+      if (response.ok)
+        setUserData({
+          user: {},
+          isAuth: false,
+          isAdmin: false,
+          pfpUrl: "",
+        });
     });
   };
 
@@ -31,26 +38,37 @@ function AuthProvider({ children }) {
     router.push(`${process.env.NEXT_PUBLIC_API_URL}/v1/oauth/${provider}`);
   };
 
-  const updateUser = () => {
-    fetch(`${process.env.NEXT_PUBLIC_API_URL}/v1/oauth/user/me`, {
-      method: "GET",
-      credentials: "include",
+  const updateUser = async () => {
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_API_URL}/v1/oauth/user/me`,
+      {
+        method: "GET",
+        credentials: "include",
+      }
+    );
+
+    if (!response.ok) return;
+    const data = await response.json();
+
+    setUserData({
+      user: data?.user,
+      isAuth: true,
+      isAdmin: data?.user?.isAdmin,
+      pfpUrl: data?.pfpUrl,
     })
-      .then((response) => {
-        if (!response.ok) return;
-        return response.json();
-      })
-      .then((data) => {
-        setData(data);
-        if (data !== undefined) setIsAuth(true);
-      });
   };
+
+  useEffect(() => {
+    updateUser();
+  }, []);
 
   return (
     <AuthContext.Provider
       value={{
-        data: data,
-        isAuth: isAuth,
+        user: userData.user,
+        isAuth: userData.isAuth,
+        isAdmin: userData.isAdmin,
+        pfpUrl: userData.pfpUrl,
         logout: logout,
         redirectToProvider: redirectToProvider,
         updateUser: updateUser,
